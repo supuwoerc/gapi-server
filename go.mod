22e56f9c1584f96f4a0d9eaa1d7ac1d9bee5205d
supuwoerc <zhangzhouou@gmail.com>
2026-05-23 10:09:49 +0800

    feat: add platform-specific graceful shutdown with gracehttp for linux

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gapi-server
 go 1.26.1
 
 require (
+	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434
 	github.com/gin-gonic/gin v1.12.0
 	github.com/google/wire v0.7.0
 	github.com/spf13/viper v1.21.0
@@ -22,6 +23,13 @@ require (
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
+	github.com/facebookgo/ensure v0.0.0-20200202191622-63f1cf65ac4c // indirect
+	github.com/facebookgo/freeport v0.0.0-20150612182905-d4adf43b75b9 // indirect
+	github.com/facebookgo/httpdown v0.0.0-20180706035922-5979d39b15c2 // indirect
+	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
+	github.com/facebookgo/stats v0.0.0-20151006221625-1b76add642e4 // indirect
+	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -11,21 +11,33 @@ import (
 
 	"gapi-server/internal/config"
 
+	"github.com/facebookgo/grace/gracehttp"
 	"go.uber.org/zap"
 )
 
+var isLinux = false
+
 type HttpServer struct {
-	server *http.Server
-	logger *zap.Logger
+	server  *http.Server
+	logger  *zap.Logger
+	isLinux bool
 }
 
 func NewHttpServer(cfg *config.ServerConfig, handler http.Handler, logger *zap.Logger) *HttpServer {
 	addr := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
 	srv := &http.Server{Addr: addr, Handler: handler}
-	return &HttpServer{server: srv, logger: logger}
+	return &HttpServer{server: srv, logger: logger, isLinux: isLinux}
 }
 
 func (s *HttpServer) Run() {
+	if s.isLinux {
+		s.graceRunServe()
+	} else {
+		s.runServer()
+	}
+}
+
+func (s *HttpServer) runServer() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 	go func() {
@@ -41,4 +53,12 @@ func (s *HttpServer) Run() {
 		s.logger.Error("server shutdown error", zap.Error(err))
 	}
 	s.logger.Info("server stopped")
+}
+
+func (s *HttpServer) graceRunServe() {
+	s.logger.Info("grace server running", zap.String("addr", s.server.Addr))
+	if err := gracehttp.Serve(s.server); err != nil {
+		s.logger.Fatal("grace server error", zap.Error(err))
+	}
+	s.logger.Info("grace server stopped")
 }
